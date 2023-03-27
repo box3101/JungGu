7b28ecc4e50ec1547f47cb33c18ff8d88526b9fd
cylee_ispark <tony.at.cp@gmail.com>
2023-03-27 14:18:39 +0900

    20230327 수정사항 반영

--- assets/front/js/main.js
+++ assets/front/js/main.js
@@ -203,18 +203,18 @@
     const $sharingSpaceList = document.querySelector("#sharingSpaceList");
 
     const changeTabIndex = () => {
-      $$swiperAll.forEach((el) => {
-        const $swiperAnchor = el.querySelector("a");
-        if (el.classList.contains("swiper-slide-active")) {
-          $swiperAnchor.setAttribute("tabindex", "0");
-        } else {
-          $swiperAnchor.setAttribute("tabindex", "-1");
-        }
-      });
-      const $$swiperSlideDuplicateAll = document.querySelectorAll(".swiper-slide-duplicate a");
-      $$swiperSlideDuplicateAll.forEach((el) => {
-        el.setAttribute("tabindex", "-1");
-      });
+      if (false) {
+        $$swiperAll.forEach((el) => {
+          const $swiperAnchor = el.querySelector("a");
+          if (el.classList.contains("swiper-slide-active")) {
+            $swiperAnchor.setAttribute("tabindex", "0");
+          }
+        });
+        const $$swiperSlideDuplicateAll = document.querySelectorAll(".swiper-slide-duplicate a");
+        $$swiperSlideDuplicateAll.forEach((el) => {
+          el.setAttribute("tabindex", "-1");
+        });
+      }
     }
 
     // visualSlide
@@ -227,8 +227,50 @@
         loop: true,
         // effect: 'fade',
         navigation: {
-          nextEl: '.swiper-button-next', // 다음 버튼 클래스명
-          prevEl: '.swiper-button-prev', // 이번 버튼 클래스명
+          nextEl: '#visualSlide .swiper-button-next', // 다음 버튼 클래스명
+          prevEl: '#visualSlide .swiper-button-prev', // 이번 버튼 클래스명
+        },
+        a11y: {
+          prevSlideMessage: '이전 슬라이드',
+          nextSlideMessage: '다음 슬라이드',
+          slideLabelMessage: '총 {{slidesLength}}장의 슬라이드 중 {{index}}번 슬라이드 입니다.',
+        },
+        on: {
+          init: function () {
+            thisSlide = this;
+            autoPlayBtn = document.querySelector('.autoplayControl > button');
+            autoPlayBtn.addEventListener('click', () => {
+              autoPlayState = autoPlayBtn.getAttribute('aria-pressed');
+              if (autoPlayState === 'false') {
+                autoPlayBtn.setAttribute('aria-pressed', 'true');
+                thisSlide.autoplay.stop();
+              } else if (autoPlayState === 'true') {
+                autoPlayBtn.setAttribute('aria-pressed', 'false');
+                thisSlide.autoplay.start();
+              };
+            });
+            changeTabIndex();
+          },
+          activeIndexChange: function () {
+            this.realIndex += 1;
+            $currentNum.textContent = `0${this.realIndex}`;
+            $allNum.textContent = `0${$$swiperAll.length}`;
+          },
+          slideChangeTransitionStart: () => {
+            changeTabIndex();
+          },
+        },
+      });
+
+      const moVisualSlide = new Swiper('#moVisualSlide', {
+        // autoplay: {
+        //   delay: 3000,
+        // },
+        loop: true,
+        // effect: 'fade',
+        navigation: {
+          nextEl: '#moVisualSlide .swiper-button-next', // 다음 버튼 클래스명
+          prevEl: '#moVisualSlide .swiper-button-prev', // 이번 버튼 클래스명
         },
         a11y: {
           prevSlideMessage: '이전 슬라이드',
@@ -559,16 +601,24 @@
 
   }
 
-  //상세 검색
-  const detailedSearch = () => {
-    if (document.querySelector('.searchArea')) {
-      const $detailedSearchBtn = document.querySelector(".detailedSearch");
-      $detailedSearchBtn.addEventListener("click", (e) => {
-        const target = e.target;
-        target.closest(".searchArea__info").classList.toggle("on");
-      });
-    }
+ //�곸꽭 寃���
+ const detailedSearch = () => {
+  if (document.querySelector('.searchArea')) {
+    const $detailedSearchBtn = document.querySelector(".detailedSearch");
+    $detailedSearchBtn.addEventListener("click", (e) => {
+      const target = e.target;
+      target.closest(".searchArea__info").classList.toggle("on");
+      if(target.closest(".searchArea__info").classList.contains("on")) {
+          $detailedSearchBtn.setAttribute("aria-expanded","true")
+          $(".searchArea__box>.hidden").text("열림");
+      }
+      else {
+          $detailedSearchBtn.setAttribute("aria-expanded","false");
+          $(".searchArea__box>.hidden").text("닫힘");
+      }
+    });
   }
+}
 
   //공통 datePicker
   $(function () {
@@ -631,5 +681,13 @@
     });
   });
 
+  // 공통 타이틀 변경 스크립트
+  (function () {
+    setTimeout(function () {
+      const subHead = document.querySelector('.subHead__tit');
+      const headTitle = subHead.textContent;
 
+      document.title = `인천중구교육포털 > ${headTitle}`;
+    }, 100)
+  })();
 })();
